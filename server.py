237e42cd249830dd5a8cc54f0d18dff9027a3bb2
CoderDrew62 <birdchirp313@gmail.com>
2025-05-27 19:00:05 -0400

    initial upload

---
+++ server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('X-Frame-Options', 'ALLOWALL')
+        super().end_headers()
+
+if __name__ == "__main__":
+    PORT = 12000
+    os.chdir('/workspace')
+    
+    with socketserver.TCPServer(("0.0.0.0", PORT), MyHTTPRequestHandler) as httpd:
+        print(f"Server running at http://0.0.0.0:{PORT}")
+        print(f"Access the game at: https://work-1-kwbmsnzhausxctuw.prod-runtime.all-hands.dev")
+        httpd.serve_forever()
